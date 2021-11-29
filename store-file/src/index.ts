8289955dfb983806cd9fbf6a0cb7c287f7a536e0
Zikun Fan <zikunvan@gmail.com>
2021-11-29 16:22:30 +0800

    Rebuild file storing sample

---
+++ store-file/src/index.ts
@@ -0,0 +1,102 @@
+import fs from 'fs';
+import path from 'path';
+import { create, IPFSHTTPClient } from 'ipfs-http-client';
+import { ethers } from 'ethers';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { typesBundleForPolkadot } from '@crustio/type-definitions';
+import { Keyring } from '@polkadot/keyring';
+
+const crustChainEndpoint = 'wss://rpc.crust.network'; // More endpoints: https://github.com/crustio/crust-apps/blob/master/packages/apps-config/src/endpoints/production.ts#L9
+const ipfsW3GW = 'https://crustipfs.xyz'; // More web3 authed gateways: https://github.com/crustio/ipfsscan/blob/main/lib/constans.ts#L29
+const crustSeeds = 'man laptop chimney debate grit impact fee pony exchange away ritual lady'; // Create account(seeds): https://wiki.crust.network/docs/en/crustAccount
+const api = new ApiPromise({
+    provider: new WsProvider(crustChainEndpoint),
+    typesBundle: typesBundleForPolkadot,
+});
+
+main();
+
+async function main() {
+    // I. Upload file to IPFS
+    // 1. Read file
+    const filePath = 'sampleFile.txt';
+    const fileContent = await fs.readFileSync(path.resolve(__dirname, filePath));
+
+    // 2. [Local] Create IPFS instance
+    const ipfsLocal = await create({ url: 'http://localhost:5001' });
+
+    // 2. [Gateway] Create IPFS instance
+    // Now support: ethereum-series, polkadot-series, solana, elrond, flow, near, ...
+    // Let's take ethereum as example
+    const pair = ethers.Wallet.createRandom();
+    const sig = await pair.signMessage(pair.address);
+    const authHeaderRaw = `eth-${pair.address}:${sig}`;
+    const authHeader = Buffer.from(authHeaderRaw).toString('base64');
+    const ipfsRemote = create({
+        url: `${ipfsW3GW}/api/v0`,
+        headers: {
+            authorization: `Basic ${authHeader}`
+        }
+    });
+
+    // 3. Add IPFS
+    const rst = await addFile(ipfsRemote, fileContent); // Or use IPFS local
+    console.log(rst);
+
+    // II. Place storage order
+    await placeStorageOrder(rst.cid, rst.size);
+
+    // III. Query storage status
+    // Query forever here ...
+    while (true) {
+        const orderStatus: any = (await getOrderState(rst.cid)).toJSON();
+        console.log('Replica count: ', orderStatus['reported_replica_count']); // Print the replica count
+        await new Promise(f => setTimeout(f, 1500)); // Just wait 1.5s for next chain-query
+    }
+}
+
+async function addFile(ipfs: IPFSHTTPClient, fileContent: any) {
+    // 1. Add file to ipfs
+    const cid = await ipfs.add(fileContent);
+
+    // 2. Get file status from ipfs
+    const fileStat = await ipfs.files.stat("/ipfs/" + cid.path);
+
+    return {
+        cid: cid.path,
+        size: fileStat.cumulativeSize
+    };
+}
+
+async function placeStorageOrder(fileCid: string, fileSize: number) {
+    // 1. Construct place-storage-order tx
+    const tips = 0;
+    const memo = '';
+    const tx = api.tx.market.placeStorageOrder(fileCid, fileSize, tips, memo);
+
+    // 2. Load seeds(account)
+    const kr = new Keyring({ type: 'sr25519' });
+    const krp = kr.addFromUri(crustSeeds);
+
+    // 3. Send transaction
+    await api.isReadyOrError;
+    const unsub = await tx.signAndSend(krp, ({events = [], status}) => {
+        console.log(`💸  Tx status: ${status.type}, nonce: ${tx.nonce}`);
+
+        if (status.isInBlock) {
+            events.forEach(({event: {method, section}}) => {
+                if (method === 'ExtrinsicSuccess') {
+                    console.log(`✅  Place storage order success!`);
+                    unsub();
+                }
+            });
+        } else {
+            // Pass it
+        }
+    });
+}
+
+async function getOrderState(cid: string) {
+    await api.isReadyOrError;
+    return await api.query.market.files(cid);
+}
